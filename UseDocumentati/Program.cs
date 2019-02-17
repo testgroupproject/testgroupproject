4b72a5cbb75b5aeea021071860c209ae28bde855
Velizar Vasilev <veli_vasilev_@abv.bg>
2019-02-17 13:37:15 +0200

    new var

--- UseDocumentati/Program.cs
+++ UseDocumentati/Program.cs
@@ -33,6 +33,7 @@ namespace UseDocumentati
         static void Main(string[] args)
         {
             Console.WriteLine(GetZero());
+            int Zero = GetZero();
             
         }
     }
